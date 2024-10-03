9544221a91669384871f6c5567aadb89955c46f4
wuliuer <2039900132@qq.com>
2024-10-03 21:50:35 +0800

    ignore intercpter

--- src/main/java/com/example/demo/Config/WebConfig.java
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -1,10 +1,14 @@
 package com.example.demo.Config;
 
 import com.example.demo.Interceptor.LoginInterceptor;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,10 +16,25 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     LoginInterceptor loginInterceptor;
 
+    @Bean
+    public OpenAPI springShopOpenApi() {
+        return new OpenAPI()
+                // 接口文档标题
+                .info(new Info().title("Moodiary接口文档")
+                        // 接口文档简介
+                        .description("Moodiary后端开发接口文档")
+                        // 接口文档版本
+                        .version("1.0")
+                        // 开发者联系方式
+                        .contact(new Contact().name("ISS - 7")
+                        ));
+
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        WebMvcConfigurer.super.addInterceptors(registry);
-        registry.addInterceptor(loginInterceptor).excludePathPatterns("/users/login","/users/register","/users/addUser");
+        registry.addInterceptor(loginInterceptor)
+                .excludePathPatterns("/users/login","/users/addUser");
     }
 }
