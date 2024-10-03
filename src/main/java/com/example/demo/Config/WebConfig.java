9059f144602fa59f3ae1c15e4eeacead44c3171e
wuliuer <2039900132@qq.com>
2024-10-03 22:30:04 +0800

    add the test mode in LoginInterceptor

--- src/main/java/com/example/demo/Config/WebConfig.java
+++ src/main/java/com/example/demo/Config/WebConfig.java
@@ -31,6 +31,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     }
 
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        WebMvcConfigurer.super.addInterceptors(registry);
