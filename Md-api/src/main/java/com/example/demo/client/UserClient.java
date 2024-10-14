9a520c1efb8faf588fd1a355cdafa34beabd9585
CH4NGEbetter <1468740331@qq.com>
2024-10-14 18:05:18 +0800

    test

--- Md-api/src/main/java/com/example/demo/client/UserClient.java
+++ Md-api/src/main/java/com/example/demo/client/UserClient.java
@@ -3,13 +3,13 @@ package com.example.demo.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import javax.xml.transform.Result;
+import com.example.demo.common.Utils.Result;
+//import javax.xml.transform.Result;
 
 
 @FeignClient("User-service")
 public interface UserClient {
 
-    @GetMapping("/getUser/{id}")
-    Result getUser(@PathVariable Integer id);
+    @GetMapping("/users/getUser/{id}")
+    Result getUser(@PathVariable("id") Integer id);
 }
