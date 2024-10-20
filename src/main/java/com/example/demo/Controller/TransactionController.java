7f7999f0bbca2ab64665f3f619cb38ea4b60d89b
StevenZhang2002 <zhangbowen0702@sina.com>
2024-10-20 11:30:17 +0800

    add check transaction history

--- src/main/java/com/example/demo/Controller/TransactionController.java
+++ src/main/java/com/example/demo/Controller/TransactionController.java
@@ -1,15 +1,16 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.PageBean;
 import com.example.demo.Service.TransactionService;
 import com.example.demo.Utils.Result;
+import com.example.demo.Utils.ThreadLocalUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -23,12 +24,30 @@ public class TransactionController {
     // 创建积分交易记录
     @Operation(summary = "创建积分交易记录")
     @PostMapping("/create")
-    public Result createTransaction(@RequestParam Integer userId,
+    public Result createTransaction(
                                     @RequestParam Integer changeAmount,
                                     @RequestParam String transactionType,
                                     @RequestParam String description) {
+        Map<String, Object>claims = ThreadLocalUtil.get();
+        int userId = (int)claims.get("id");
         log.info("创建交易记录：userId={}, changeAmount={}, transactionType={}", userId, changeAmount, transactionType);
         transactionService.createTransaction(userId, changeAmount, transactionType, description);
         return Result.success();
     }
+
+
+    @Operation(summary = "获取积分交易记录")
+    @GetMapping("/history")
+    public Result getTransactionHistory(@RequestParam(defaultValue = "1") Integer page,
+                                        @RequestParam(defaultValue = "10") Integer pageSize){
+        Map<String, Object>claims = ThreadLocalUtil.get();
+        int userId = (int)claims.get("id");
+        PageBean pageBean = transactionService.page(userId,page, pageSize);
+        //响应
+        return Result.success(pageBean);
+
+    }
+
+
+
 }
--- src/main/java/com/example/demo/Service/TransactionService.java
+++ src/main/java/com/example/demo/Service/TransactionService.java
@@ -1,5 +1,11 @@
 package com.example.demo.Service;
 
+import com.example.demo.Entity.PageBean;
+
 public interface TransactionService {
     void createTransaction(Integer userId, Integer changeAmount, String transactionType, String description);
+
+
+    PageBean page(int userId, Integer page, Integer pageSize);
+
 }
