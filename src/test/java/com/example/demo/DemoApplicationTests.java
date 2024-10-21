b01361721a7d7917ad76f60cd566c83a30bbe8e0
StevenZhang2002 <zhangbowen0702@sina.com>
2024-10-21 11:09:31 +0800

    update track

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,19 +1,19 @@
-package com.example.demo;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-
-    @Test
-    void testAddUser(){
-
-    }
-
-}
+//package com.example.demo;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class DemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//
+//    @Test
+//    void testAddUser(){
+//
+//    }
+//
+//}
