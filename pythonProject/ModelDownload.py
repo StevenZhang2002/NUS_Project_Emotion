d547b859333cc7bed030f665e096c5b09f640ff7
StevenZhang2002 <98020035+StevenZhang2002@users.noreply.github.com>
2024-11-11 19:48:11 +0800

    Delete pythonProject directory

--- pythonProject/ModelDownload.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline
-
-# 下载模型并保存到本地
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions")
-classifier.save_pretrained('./roberta-go_emotions')
